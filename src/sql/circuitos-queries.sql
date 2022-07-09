a2eac01905acfcdd3864501c9f4273b0efee9a4e
Ellen Maria <ellenmariadev@gmail.com>
2022-07-09 16:17:31 -0300

    Update circuitos-queries.sql

--- src/sql/circuitos-queries.sql
+++ src/sql/circuitos-queries.sql
@@ -2,15 +2,15 @@
 SELECT 
 	C.name as circuito,
 	C.country,
-    S.status, 
-    count(*) as acidentes
+	S.status, 
+	count(*) as acidentes
 FROM f1_circuits as C
 LEFT JOIN 
 	f1_races as RA on C.circuitId = RA.circuitId
 LEFT JOIN 
 	f1_results as RE on RA.raceId = RE.raceId
 LEFT JOIN 
-    f1_status as S on S.statusId = RE.statusId
+    	f1_status as S on S.statusId = RE.statusId
 GROUP BY circuito, C.country, S.status
 HAVING S.status = 'Accident' 
 ORDER by acidentes desc;
@@ -19,10 +19,10 @@ ORDER by acidentes desc;
 -- STATUS DAS CORRIDAS DO LEWIS HAMILTON
 SELECT 
 	D.forename,
-    D.surname,
-    C.name as circuito,
-    RE.statusId,
-    S.status
+	D.surname,
+	C.name as circuito,
+	RE.statusId,
+	S.status
 FROM f1_circuits as C 
 LEFT JOIN 
 	f1_races as RA on C.circuitId = RA.circuitId
@@ -32,5 +32,5 @@ LEFT JOIN
 	f1_drivers as D on RE.driverId = D.driverId
 LEFT JOIN 
 	f1_status as S on RE.statusId = S.statusId
-WHERE  D.forename = 'Lewis' and D.surname = 'Hamilton'
+WHERE D.forename = 'Lewis' and D.surname = 'Hamilton'
 GROUP BY D.forename, D.surname, RE.statusId, S.status, circuito;
